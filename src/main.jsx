6299e7003497470137917b236493067311e02548
AnselRamosOcampo <uniramosocampoea@gmail.com>
2025-06-14 22:55:44 -0400

    Configuración final para GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import React from 'react'
 import reactDOM from 'react-dom/client'
 import Parcial from './Parcial.jsx'
-import { BrowserRouter } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 createRoot(document.getElementById('root')).render(
